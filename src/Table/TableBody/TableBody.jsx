57f19d89bb34174732ce632110c82982289098b0
umayat <umayat@avenuecode.com>
2019-07-04 13:03:31 -0400

    feat: add TableBody component

--- src/Table/TableBody/TableBody.jsx
+++ src/Table/TableBody/TableBody.jsx
@@ -1,5 +1,27 @@
 // @flow
+import React from 'react';
+import TableRow from '../TableRow';
 
-const TableBody = null;
+type Props = {
+  rowData: Array<any>,
+  CustomTableRow?: React.Component
+}
+
+const TableBody = (props: Props) => {
+  const { rowData, CustomTableRow } = props;
+
+  return (
+    <tbody>
+      <TableRow
+        rowData={rowData}
+        CustomTableRow={CustomTableRow}
+      />
+    </tbody>
+  );
+};
+
+TableBody.defaultProps = {
+  CustomTableRow: null,
+};
 
 export default TableBody;
--- src/Table/TableRow/TableRow.jsx
+++ src/Table/TableRow/TableRow.jsx
@@ -3,19 +3,32 @@ import React from 'react';
 import uuidv4 from 'uuid/v4';
 
 type Props = {
-  rowData: Array<Object>
+  rowData: Array<any>,
+  CustomTableRow?: React.Component
 }
 
 const TableRow = (props: Props) => {
-  const { rowData } = props;
+  const { rowData, CustomTableRow } = props;
 
   return (
-    <tr>
+    <>
       {
-        rowData.map(data => <td key={uuidv4()}>{data}</td>)
+        CustomTableRow ? (
+          <CustomTableRow key={uuidv4()} rowData={rowData} />
+        ) : (
+          <tr>
+            {
+              rowData.map(data => <td key={uuidv4()}>{data}</td>)
+            }
+          </tr>
+        )
       }
-    </tr>
+    </>
   );
+};
+
+TableRow.defaultProps = {
+  CustomTableRow: null,
 };
 
 export default TableRow;
