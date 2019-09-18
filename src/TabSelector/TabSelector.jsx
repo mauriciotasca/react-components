5c89d1ad548e82c3995048cdcade45e7c5c4b8c7
Camila Rodrigues <camila.rodrigues@avenuecode.com>
2019-09-18 16:43:49 -0300

    ACDS-9 Porting Tab Selector component from ACDC.

---
+++ src/TabSelector/TabSelector.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './TabSelector.scss';
+
+type Props = {
+  /** Array of React Component to load the `nav-items` inside `nav-tabs` element */
+  tabItems: Array<React.Component>,
+  /** Boolean to show or not the animation of tab transition. Default is true. */
+  hasTabTransitionAnimation?: Boolean
+};
+
+const TabSelector = (props: Props) => {
+  const { tabItems, hasTabTransitionAnimation } = props;
+
+  const indexOfActiveTab = tabItems.findIndex(
+    tab => tab.props.className.indexOf('active') !== -1
+  );
+  const maxTransform = (tabItems.length - 1) * 100;
+  const transformPosition = maxTransform - 100 * indexOfActiveTab;
+  const widthBar = 100 / tabItems.length;
+
+  /**
+   * The Style of tab Animation (current-segment) depends of the quantity of tab.
+   * And it needs to know how the tab is activated.
+   */
+  const tabAnimationStyle = {
+    width: `${widthBar}%`,
+    transform: `translateX(-${transformPosition}%)`
+  };
+
+  return (
+    <div
+      className="ac-tab-selector nav nav-tabs position-relative d-flex justify-content-end"
+      role="tablist"
+    >
+      {tabItems.map(tabItem => (
+        <div
+          className="nav-item text-center text-uppercase font-weight-bold"
+          key={tabItem.key}
+        >
+          {tabItem}
+        </div>
+      ))}
+
+      {hasTabTransitionAnimation && (
+        <div className="current-segment" style={tabAnimationStyle} />
+      )}
+    </div>
+  );
+};
+
+TabSelector.defaultProps = {
+  hasTabTransitionAnimation: true
+};
+
+export default TabSelector;
---
+++ src/TabSelector/TabSelector.spec.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import TabSelector from './TabSelector';
+
+const NAV_SELECTOR = '.ac-tab-selector.nav';
+const NAV_ITEMS_SELECTOR = '.nav-item';
+const DIV_TAB_TRANSITION_ANIMATION = '.current-segment';
+const HOME_TAB = '.nav-item #home';
+const PROFILE_TAB = '.nav-item #home';
+const ACTIVE_CLASS = 'active';
+
+const defaultProps = {
+  tabItems: []
+};
+
+describe('Tab Selector Component', () => {
+  const wrapper = mount(<TabSelector {...defaultProps} />);
+
+  it('should render without error', () => {
+    expect(wrapper).toBeTruthy();
+  });
+});
+
+describe('When Tab Selector is rendered with tab items', () => {
+  const tabItemsData = [
+    {
+      id: 'home',
+      path: '#nav-home',
+      label: 'Home',
+      content: (
+        <div>
+          <h2>Home Content</h2>
+        </div>
+      )
+    },
+    {
+      id: 'profile',
+      path: '#nav-profile',
+      label: 'Profile',
+      content: (
+        <div>
+          <h2>Profile Content</h2>
+        </div>
+      )
+    },
+    {
+      id: 'contact',
+      path: '#nav-contact',
+      label: 'Contact',
+      content: (
+        <div>
+          <h2>Contact Content</h2>
+        </div>
+      )
+    }
+  ];
+
+  let currentTab = tabItemsData[0].id;
+
+  const tabItems = tabItemsData.map(tabData => (
+    <a
+      className={`nav-link ${currentTab === tabData.id ? 'active' : ''}`}
+      id={tabData.id}
+      key={tabData.id}
+      data-toggle="tab"
+      href={tabData.path}
+      role="tab"
+      onClick={() => {
+        currentTab = tabData.id;
+      }}
+    >
+      {tabData.label}
+    </a>
+  ));
+
+  defaultProps.tabItems = tabItems;
+
+  const wrapper = mount(<TabSelector {...defaultProps} />);
+
+  it('should contain a nav element', () => {
+    expect(wrapper.find(NAV_SELECTOR)).toHaveLength(1);
+  });
+
+  it('should contain a nav list when passed in naviagtaion items', () => {
+    expect(wrapper.find(NAV_ITEMS_SELECTOR).length).toEqual(tabItems.length);
+  });
+
+  it('should contain a div element with class "current-segment" that is used to do the animation of transition between tabs, which is optional but true as default', () => {
+    expect(wrapper.find(DIV_TAB_TRANSITION_ANIMATION)).toHaveLength(1);
+  });
+
+  it('the active class before any tab change should be in the first one (home tab in this case)', () => {
+    expect(wrapper.find(HOME_TAB).prop('className')).toContain(ACTIVE_CLASS);
+  });
+
+  it('the active class should change to the clicked tab', () => {
+    wrapper.find(PROFILE_TAB).simulate('click');
+    expect(wrapper.find(HOME_TAB).prop('className')).toContain(ACTIVE_CLASS);
+  });
+
+  it('when tab transition animation is disabled', () => {
+    const hasTabTransitionAnimation = false;
+
+    wrapper.setProps({ hasTabTransitionAnimation });
+
+    expect(wrapper.find(DIV_TAB_TRANSITION_ANIMATION).length).toEqual(0);
+  });
+});
